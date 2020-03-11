7dcbbb582e53a4e6ab2d7762ff4658f214c3020d
narren <narren@gmail.com>
2020-03-11 18:58:34 +0530

    worked on profile and feed back api calls

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -1,127 +1,125 @@
 import * as types from './../constants/types';
 import Axios from 'axios';
-import { API_BASE_URL } from './../constants/config';
-
+import {API_BASE_URL} from './../constants/config';
 
 //SIGNUP
 const signupLoading = () => ({
-    type: types.SIGNUP_STARTED
+  type: types.SIGNUP_STARTED,
 });
 
-const signupSuccess = (data) => ({
-    type: types.SIGNUP_SUCCESS,
-    payload: data
+const signupSuccess = data => ({
+  type: types.SIGNUP_SUCCESS,
+  payload: data,
 });
 
-const signupFailure = (error) => ({
-    type: types.SIGNUP_FAILURE,
-    payload: error
+const signupFailure = error => ({
+  type: types.SIGNUP_FAILURE,
+  payload: error,
 });
 
 //LOGIN
 const loginLoading = () => ({
-    type: types.LOGIN_STARTED
+  type: types.LOGIN_STARTED,
 });
 
-const loginSuccess = (data) => ({
-    type: types.LOGIN_SUCCESS,
-    payload: data
+const loginSuccess = data => ({
+  type: types.LOGIN_SUCCESS,
+  payload: data,
 });
 
-const loginFailure = (error) => ({
-    type: types.LOGIN_FAILURE,
-    payload: error
+const loginFailure = error => ({
+  type: types.LOGIN_FAILURE,
+  payload: error,
 });
-
 
 //FETCH PROFILE
 const fetchUserProfileLoading = () => ({
-    type: types.FETCH_PROFILE_LOADING
+  type: types.FETCH_PROFILE_LOADING,
 });
 
-const fetchUserProfileSuccess = (data) => ({
-    type: types.FETCH_PROFILE_SUCCESS,
-    payload: data
+const fetchUserProfileSuccess = data => ({
+  type: types.FETCH_PROFILE_SUCCESS,
+  payload: data,
 });
 
-const fetchUserProfileFailure = (error) => ({
-    type: types.FETCH_PROFILE_FAILURE,
-    payload: error
+const fetchUserProfileFailure = error => ({
+  type: types.FETCH_PROFILE_FAILURE,
+  payload: error,
 });
-
 
 //UPDATE PROFILE
 const updateUserProfileLoading = () => ({
-    type: types.UPDATE_PROFILE_LOADING
+  type: types.UPDATE_PROFILE_LOADING,
 });
 
-const updateUserProfileSuccess = (data) => ({
-    type: types.UPDATE_PROFILE_SUCCESS,
-    payload: data
+const updateUserProfileSuccess = data => ({
+  type: types.UPDATE_PROFILE_SUCCESS,
+  payload: data,
 });
 
-const updateUserProfileFailure = (error) => ({
-    type: types.UPDATE_PROFILE_FAILURE,
-    payload: error
+const updateUserProfileFailure = error => ({
+  type: types.UPDATE_PROFILE_FAILURE,
+  payload: error,
 });
 
+export const signupUser = bodyParams => dispatch => {
+  dispatch(signupLoading());
+  const url = `${API_BASE_URL}/customers/signup`;
+  return Axios.post(url, bodyParams)
+    .then(res => {
+      console.log('SUCCESS');
+      Axios.defaults.headers.Authorization = res.data.token;
+      dispatch(signupSuccess(res.data));
+      return res;
+    })
+    .catch(err => {
+      console.log('Error =>', err.toString());
+      dispatch(signupFailure(err));
+      throw err;
+    });
+};
 
-
-export const signupUser = (bodyParams) => dispatch => {
-    dispatch(signupLoading());
-    const url = `${API_BASE_URL}/customers/signup`;
-    return Axios.post(url, bodyParams)
-        .then(res => {
-            console.log('SUCCESS')
-            Axios.defaults.headers.Authorization = res.data.token;
-            dispatch(signupSuccess(res.data));
-            return res;
-        })
-        .catch(err => {
-            console.log('Error =>', err.toString())
-            dispatch(signupFailure(err));
-            throw err;
-        })
-}
-
-export const loginUser = (bodyParams) => dispatch => {
-    dispatch(loginLoading());
-    const url = `${API_BASE_URL}/customers/signin`;
-    return Axios.post(url, bodyParams)
-        .then(res => {
-            console.log('LOGON RES =>', res.data)
-            Axios.defaults.headers.Authorization = res.data.token;
-            dispatch(loginSuccess(res.data));
-            return res;
-        })
-        .catch(err => {
-            console.log('Error =>', err)
-            dispatch(loginFailure(err));
-            throw err;
-        })
-}
+export const loginUser = bodyParams => dispatch => {
+  dispatch(loginLoading());
+  const url = `${API_BASE_URL}/customers/signin`;
+  return Axios.post(url, bodyParams)
+    .then(res => {
+      console.log('LOGON RES =>', res.data);
+      Axios.defaults.headers.Authorization = res.data.token;
+      dispatch(loginSuccess(res.data));
+      return res;
+    })
+    .catch(err => {
+      console.log('Error =>', err);
+      dispatch(loginFailure(err));
+      throw err;
+    });
+};
 
 export const fetchUserProfile = () => dispatch => {
-    dispatch(fetchUserProfileLoading());
-    const url = `${API_BASE_URL}/customers/profile`;
-    Axios.get(url)
-        .then(res => {
-            console.log('PROFILE =>', res.data)
-            dispatch(fetchUserProfileSuccess(res.data));
-        })
-        .catch(err => {
-            dispatch(fetchUserProfileFailure(err));
-        })
-}
+  dispatch(fetchUserProfileLoading());
+  const url = `${API_BASE_URL}/customers/profile`;
+  Axios.get(url)
+    .then(res => {
+      console.log('PROFILE =>', res.data);
+      dispatch(fetchUserProfileSuccess(res.data));
+    })
+    .catch(err => {
+      dispatch(fetchUserProfileFailure(err));
+    });
+};
 
-export const updateUserProfile = (profileData) => dispatch => {
-    dispatch(updateUserProfileLoading());
-    const url = `${API_BASE_URL}/customers/profile/update`;
-    Axios.post(url, profileData)
-        .then(res => {
-            dispatch(updateUserProfileSuccess(res.data));
-        })
-        .catch(err => {
-            dispatch(updateUserProfileFailure(err));
-        })
-}
+export const updateUserProfile = profileData => dispatch => {
+  dispatch(updateUserProfileLoading());
+  const url = `${API_BASE_URL}/customers/profile/update`;
+  return Axios.put(url, profileData)
+    .then(res => {
+      dispatch(fetchUserProfile());
+      // dispatch(updateUserProfileSuccess(res.data));
+      return res;
+    })
+    .catch(err => {
+      dispatch(updateUserProfileFailure(err));
+      throw err;
+    });
+};
